c0f9d3f5643f1f859bb8ac332844c3de3eba6548
Arthur <arthurantero@hotmail.com>
2023-01-09 00:31:24 -0300

    Teste 1

--- testa_oito_rainhas.py
+++ testa_oito_rainhas.py
@@ -1,1 +1,11 @@
 from oito_rainhas import *
+
+def teste_TabuleiroEh8x8():
+    entrada_valida = [[0,0,0,0,1,0,0,0],
+                      [0,1,0,0,0,0,0,0],
+                      [0,0,0,1,0,0,0,0],
+                      [0,0,0,0,0,0,1,0],
+                      [0,0,1,0,0,0,0,0],
+                      [0,0,0,0,0,0,0,1],
+                      [1,0,0,0,0,0,0,0]]
+    assert TabuleiroEh8x8(entrada_valida) != -1
