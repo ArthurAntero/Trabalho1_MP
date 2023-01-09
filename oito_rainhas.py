430cc4be2e86beda316d9398c7ab786455554ea0
Arthur <arthurantero@hotmail.com>
2023-01-09 01:31:12 -0300

    Otimizando a primeira funcao

--- oito_rainhas.py
+++ oito_rainhas.py
@@ -2,9 +2,8 @@ def TabuleiroEh8x8(entrada):
     linhas = len(entrada)
     if linhas != 8:
         return -1
-    else:
-        for i in range(len(entrada)):
-            colunas = len(entrada[i])
-            if colunas != 8:
-                return -1
+    for i in range(len(entrada)):
+        colunas = len(entrada[i])
+        if colunas != 8:
+            return -1
     return 'ok'
